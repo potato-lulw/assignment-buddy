5dd1f18c5c2d669136a441992dd8df15d99a8ea8
potato-lulw <om.patil17072@gmail.com>
2024-01-26 16:59:23 +0530

    🐛 Actually fixed the white space issue

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Sidebar from '../components/sidebar';
 import { FaAngleRight } from "react-icons/fa6";
+import AssignmentDetails from '../components/assignment-details';
 
 const Home = () => {
   const [hidden, setHidden] = useState(true);
@@ -13,18 +14,18 @@ const Home = () => {
     <div className="home flex md:flex-row flex-col flex-wrap">
       {/* Button for toggling sidebar on small screens */}
       <button
-        className="md:hidden p-2 bg-blue-500 text-white flex align-middle min-w-0 items-center justify-center gap-2"
+        className="md:hidden p-2 bg-blue-500 text-white flex align-middle min-w-0 items-center justify-center gap-2 w-full"
         onClick={toggleSidebar}
       >
         Subjects <FaAngleRight className={`${hidden ? 'rotate-0 transition': "rotate-90 transition"} `}/>
       </button>
 
-      <div className={`w-full md:w-1/4 md:m-4 md:block ${hidden ? 'hidden' : ''}`}>
+      <div className={`w-full md:w-1/4 md:mx-4 mt-4 md:block ${hidden ? 'hidden' : ''}`}>
         <Sidebar />
       </div>
 
-      <div className="w-[100%] flex-1 overflow-hidden border m-4">
-        Main div
+      <div className="w-[100%] flex-1 overflow-hidden border ">
+        <AssignmentDetails  />
       </div>
     </div>
   );
