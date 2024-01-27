b4ee4ecb0682ec0cd8ed01b5bd219083e502abbf
potato-lulw <om.patil17072@gmail.com>
2024-01-27 19:57:27 +0530

    🐛 Fixed view button on card's href

--- frontend/src/components/assignment-card.js
+++ frontend/src/components/assignment-card.js
@@ -6,11 +6,11 @@ const AssignmentCard = ({ assignments }) => {
       {Object.values(assignments).map((assignment) => (
         <div key={assignment.number} className="mb-4  p-8 rounded-md bg-primary text-secondary w-full md:w-1/2 lg:w-1/3 xl:w-1/4 gap-2 flex flex-col">
           <h3 className="text-xl font-bold text-primary">{`Assignment: ${assignment.number}`}</h3>
-          <p className="text-primary">{`Unit: ${assignment.subjectPrefix}`}</p>
+          <p className="text-primary">{`Unit: ${assignment.number}`}</p>
           <p className="text-primary">{`Given on: ${assignment.givenOn}`}</p>
           <p className="text-primary">{`Submission date: ${assignment.submission}`}</p>
           <button className="bg-secondary text-primary px-4 py-2 rounded">
-            <a href={assignment.path} target="_blank" rel="noopener noreferrer">
+            <a href={`http://65.0.14.141:4000/api/view/${assignment.subjectPrefix}/${assignment.number}`} target="_blank" rel="noopener noreferrer">
               View Assignment
             </a>
           </button>
