aafbb458013fe37351eb0799f49641b4701c1716
potato-lulw <om.patil17072@gmail.com>
2024-01-30 08:50:00 +0530

    🥶 fixed animation on smaller ports and more

--- frontend/src/components/open-source.js
+++ frontend/src/components/open-source.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const OpenSource = () => {
     return (
-        <div className='max-w-[100vw] w-full bg-blue-500 text-center'>
-            <div className='bg-blue-500  text-center inline  relative'>
-                We are open source. Looking for contributors 👉<a href="https://github.com/potato-lulw/assignment-buddy">Github</a>
-                <div className='absolute top-2 left-[0%] bg-white w-2 h-2 blob '></div>
+        <div className='max-w-[100vw] w-full bg-blue-500  text-center changeColor'>
+            <div className='  text-center inline  relative text-sm '>
+            <a href="https://github.com/potato-lulw/assignment-buddy" className='cursor-pointer'>We are open source. Looking for contributors 👉 Github</a>
+                <div className='absolute top-2 left-[0%] bg-white w-2 h-2 blob sm:block hidden'></div>
             </div>
             
         </div>
