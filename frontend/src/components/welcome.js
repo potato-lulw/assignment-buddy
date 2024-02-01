a986108a52dbdc079c0ebdba24647e9cf50afc2d
potato-lulw <om.patil17072@gmail.com>
2024-02-01 09:39:28 +0530

    Added link to homepage button

--- frontend/src/components/welcome.js
+++ frontend/src/components/welcome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaAngleRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Welcome = ({ userName }) => {
   return (
@@ -7,12 +8,14 @@ const Welcome = ({ userName }) => {
       <h1 className="md:text-8xl text-6xl font-bold mb-3">Hi <span className='text-tertiary'>{userName}!</span></h1>
 
       <p className="text-sm mt-2 text-secondary">Please select a subject and category to get started.</p>
-
-      {/* Button with hover effect */}
-      <button className="group flex justify-center bg-primary border-dark px-4 py-2 mt-2 text-secondary hover:text-primary transition w-[80%] self-center ">
+      <p>OR</p>
+      <Link to={'/add-assignment'} className='justify-center flex'>
+      <button className="group flex justify-center bg-primary border border-dark px-4 py-2 mt-2 text-secondary hover:text-primary transition w-[80%] self-center hover:shadow-[5px_5px_0px_0px_rgba(102,252,241)]">
         Generate Assignments 
         <FaAngleRight className='self-center group-hover:translate-x-[5px] transition' />
       </button>
+      </Link>
+      
     </div>
   );
 };
